7252ab044a49ad7f2a6cee61eeb8e769c7ecd1ba
Letícia Bicalho <thdias00@hotmail.com>
2026-05-01 20:44:25 -0300

    done

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: ' http://192.168.0.153:3000/api',
-  // apiUrl: 'https://aubum-copa-back-production.up.railway.app/api',
+  // apiUrl: ' http://192.168.0.153:3000/api',
+  apiUrl: 'https://aubum-copa-back-production.up.railway.app/api',
 };
